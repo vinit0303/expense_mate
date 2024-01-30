6392ae3ed6e1b5a42160f3431b38f8ba3ea9b8ea
vinit0303 <vinitrathod7777@gmail.com>
2024-01-30 09:58:46 +0530

    a

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,9 +9,8 @@ import 'package:expense_manager/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Material app testing', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
